9e3d04e032a194ba1da0742845b19bb49acce62c
Brendan Metcalfe <bgmetcalfe@gmail.com>
2017-01-21 17:18:53 -0500

    pomodoro

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 let countdown;
-const timerDisplay = document.querySelector('.display__time-left');
-const endTime = document.querySelector('.display__end-time');
+const timerDisplay = document.querySelector('.display-time-left');
+const endTime = document.querySelector('.display-end-time');
 const buttons = document.querySelectorAll('[data-time]');
 
 function timer(seconds) {
